28a2942e29d37b7ba85aad6abc4de3a02d7d8baa
winnydafuu <jirehaguraso@gmail.com>
2023-07-05 00:19:01 +0800

    Update home.php
    I added date and year

--- admin/home.php
+++ admin/home.php
@@ -6,14 +6,26 @@
 	<head>
 		<title>Simple Employee Attendance Record System</title>
 		<?php include 'header.php'; ?>
+		<style>
+			#clock {
+				font-size: 36px;
+				transition: 0.5s;
+			}
+		</style>
 		<script>
 			function showTime() {
 				var date = new Date();
+				var month = date.toLocaleString('default', { month: 'long' }); // Get the full month name
+				var day = date.getDate(); // Get the current day
+				var year = date.getFullYear(); // Get the current year
 				var hours = date.getHours();
 				var minutes = date.getMinutes();
 				var seconds = date.getSeconds();
-				var time = hours + ":" + minutes + ":" + seconds;
-				document.getElementById("clock").innerText = time;
+				var amOrPm = hours >= 12 ? "PM" : "AM"; // Determine if it's AM or PM
+				hours = hours % 12 || 12; // Convert to 12-hour format
+				var time = hours + ":" + minutes + ":" + seconds + " " + amOrPm;
+				var dateTime = month + " " + day + ", " + year + " - " + time;
+				document.getElementById("clock").innerText = dateTime;
 				setTimeout(showTime, 1000); // Update the clock every second
 			}
 		</script>
@@ -23,7 +35,7 @@
 		<div class="container-fluid admin">
 			<div class="alert alert-primary">Dashboard</div>
 			<h5>Welcome <?php echo ucwords($user_name) ?>!</h5>
-			<p id="clock"></p> <!-- Display the clock here -->
+			<p id="clock"></p> <!-- Display the date and time here -->
 		</div>
 	</body>
 </html>
